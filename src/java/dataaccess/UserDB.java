d94ca915869790c5f80f9c021bfa3b75c0043d1c
polwu <pol-wung.yung@sait.edu.ca>
2021-11-28 22:55:40 -0700

    Remove email and password env variables

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -1,6 +1,8 @@
 package dataaccess;
 
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import models.User;
 
 
@@ -11,6 +13,30 @@ public class UserDB {
         try {
             User user = em.find(User.class, email);
             return user;
+        } finally {
+            em.close();
+        }
+    }
+    public List<User> getAll() {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            List<User> users = em.createNamedQuery("User.findAll",User.class).getResultList();
+            return users;
+        } finally {
+            em.close();
+        }
+    }
+    public void update(User user){
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        
+        try {
+            trans.begin();
+            em.merge(user);
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
         } finally {
             em.close();
         }
--- src/java/models/User.java
+++ src/java/models/User.java
@@ -35,7 +35,8 @@ import javax.xml.bind.annotation.XmlTransient;
     , @NamedQuery(name = "User.findByActive", query = "SELECT u FROM User u WHERE u.active = :active")
     , @NamedQuery(name = "User.findByFirstName", query = "SELECT u FROM User u WHERE u.firstName = :firstName")
     , @NamedQuery(name = "User.findByLastName", query = "SELECT u FROM User u WHERE u.lastName = :lastName")
-    , @NamedQuery(name = "User.findByPassword", query = "SELECT u FROM User u WHERE u.password = :password")})
+    , @NamedQuery(name = "User.findByPassword", query = "SELECT u FROM User u WHERE u.password = :password")
+    , @NamedQuery(name = "User.findByPasswordUuid", query = "SELECT u FROM User u WHERE u.passwordUuid = :passwordUuid")})
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -55,6 +56,8 @@ public class User implements Serializable {
     @Basic(optional = false)
     @Column(name = "password")
     private String password;
+    @Column(name = "password_uuid")
+    private String passwordUuid;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner", fetch = FetchType.EAGER)
     private List<Note> noteList;
     @JoinColumn(name = "role", referencedColumnName = "role_id")
@@ -114,6 +117,14 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    
+    public String getPasswordUuid() {
+        return passwordUuid;
+    }
+
+    public void setPasswordUuid(String passwordUuid) {
+        this.passwordUuid = passwordUuid;
     }
 
     @XmlTransient
